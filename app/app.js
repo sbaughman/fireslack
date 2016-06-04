a4774c46d338284c1518201eb73e59e92a108295
Seth Baughman <sbaughman1@me.com>
2016-06-04 12:40:49 -0400

    adds resolve property to login and register states to redirect to 'home' if user is already logged in

--- app/app.js
+++ app/app.js
@@ -8,27 +8,47 @@
  *
  * Main module of the application.
  */
-angular
-  .module('angularfireSlackApp', [
-    'firebase',
-    'angular-md5',
-    'ui.router'
-  ])
-  .config(function ($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'home/home.html'
-      })
-      .state('login', {
-        url: '/login',
-        templateUrl: 'auth/login.html'
-      })
-      .state('register', {
-        url: '/register',
-        templateUrl: 'auth/register.html'
-      });
+ angular
+   .module('angularfireSlackApp', [
+     'firebase',
+     'angular-md5',
+     'ui.router'
+   ])
+   .config(function ($stateProvider, $urlRouterProvider) {
+     $stateProvider
+       .state('home', {
+         url: '/',
+         templateUrl: 'home/home.html'
+       })
+       .state('login', {
+         url: '/login',
+         controller: 'AuthCtrl as authCtrl',
+         templateUrl: 'auth/login.html',
+         resolve: {
+           requireNoAuth: function($state, Auth) {
+             return Auth.$requireAuth().then(function(auth){
+               $state.go('home');
+             }, function(error) {
+               return;
+             });
+           }
+         }
+       })
+       .state('register', {
+         url: '/register',
+         controller: 'AuthCtrl as authCtrl',
+         templateUrl: 'auth/register.html',
+         resolve: {
+           requireNoAuth: function($state, Auth) {
+             return Auth.$requireAuth().then(function(auth){
+               $state.go('home');
+             }, function(error) {
+               return;
+             });
+           }
+         }
+       });
 
-    $urlRouterProvider.otherwise('/');
-  })
-  .constant('FirebaseUrl', 'https://fireslack-6df25.firebaseio.com');
+     $urlRouterProvider.otherwise('/');
+   })
+   .constant('FirebaseUrl', 'https://fireslack-6df25.firebaseio.com');
