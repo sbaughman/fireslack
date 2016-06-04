0600c4db5d2372083f8c1986a9a481686e318037
Seth Baughman <sbaughman1@me.com>
2016-06-04 12:05:22 -0400

    Adds auth controller

---
+++ app/auth/auth.controller.js
@@ -0,0 +1,18 @@
+angular.module('angularfireSlackApp')
+  .controller('AuthCtrl', function(Auth, $state){
+    var authCtrl = this;
+
+    authCtrl.user = {
+      email: '',
+      password: '',
+    };
+
+    authCtrl.login = function() {
+      Auth.$authWithPassword(authCtrl.user).then(function(auth){
+        $state.go('home');
+      }, function(error) {
+        authCtrl.error = error;
+      });
+    };
+
+  });
