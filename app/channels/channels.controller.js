62f7bdd2c7a8bd7aac9ad892f8ac29bc8c199d91
Seth Baughman <sbaughman1@me.com>
2016-06-04 19:10:34 -0400

    adds channels controller actions and logout function

--- app/channels/channels.controller.js
+++ app/channels/channels.controller.js
@@ -1,4 +1,14 @@
 angular.module('angularfireSlackApp')
   .controller('ChannelsCtrl', function($state, Auth, Users, profile, channels) {
-    var channelCtrl = this;
+    var channelsCtrl = this;
+
+    channelsCtrl.profile = profile;
+    channelsCtrl.channels = channels;
+    channelsCtrl.displayName = Users.getDisplayName;
+    channelsCtrl.getGravatar = Users.getGravatar;
+
+    channelsCtrl.logout = function() {
+      Auth.$unauth();
+      $state.go('home');
+    };
   });
