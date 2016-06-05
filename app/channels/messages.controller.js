695735ceeb7175d884675132a8615a408b8e09e2
Seth Baughman <sbaughman1@me.com>
2016-06-04 20:30:06 -0400

    adds Messages Controller

---
+++ app/channels/messages.controller.js
@@ -0,0 +1,20 @@
+angular.module('angularfireSlackApp')
+  .controller('MessagesCtrl', function(profile, channelName, messages) {
+    var messagesCtrl = this;
+
+    messagesCtrl.channelName = channelName;
+    messagesCtrl.messages = messages;
+    messagesCtrl.message = '';
+
+    messagesCtrl.sendMessage = function() {
+      if (messagesCtrl.message.length > 0) {
+        messagesCtrl.messages.$add({
+          uid: profile.$id,
+          body: messagesCtrl.message,
+          timestamp: Firebase.ServerValue.TIMESTAMP
+        }).then(function(){
+          messagesCtrl.message = '';
+        });
+      }
+    };
+  });
