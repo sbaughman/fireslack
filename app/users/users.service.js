51086e4b8d21185b0876098a7a463b197d77446f
Seth Baughman <sbaughman1@me.com>
2016-06-05 13:09:24 -0400

    Adds setOnline function to Users service to monitor what users are online

--- app/users/users.service.js
+++ app/users/users.service.js
@@ -1,6 +1,7 @@
 angular.module('angularfireSlackApp')
   .factory('Users', function($firebaseArray, $firebaseObject, FirebaseUrl) {
     var usersRef = new Firebase(FirebaseUrl + 'users');
+    var connectedRef = new Firebase(FirebaseUrl + '.info/connected');
     var users = $firebaseArray(usersRef);
 
     var Users = {
@@ -12,6 +13,18 @@ angular.module('angularfireSlackApp')
       },
       getGravatar: function(uid) {
         return '//www.gravatar.com/avatar/' + users.$getRecord(uid).emailHash;
+      },
+      setOnline: function(uid) {
+        var connected = $firebaseObject(connectedRef);
+        var online = $firebaseArray(usersRef.child(uid + '/online'));
+
+        connected.$watch(function() {
+          if (connected.$value === true) {
+            online.$add(true).then(function(connectedRef) {
+              connectedRef.onDisconnect().remove();
+            });
+          }
+        });
       },
       all: users,
     };
